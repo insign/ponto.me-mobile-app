f5957e01e276522c79794b3eb70ef5ee25df147e
Helio Oliveira <insign@gmail.com>
2013-05-29 16:44:10 -0300

    update

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
-ponto.me-mobile-app
-===================
+#Mobile app for ponto.me
 
-Mobile app for the web app ponto.me
+This project have the goal of make easier the access to ponto.me.
+
+Automatize the PhoneGap build:
+
+- 1. Open the Git Shell via Git for Windows (in project window)
+- 2. Run the command: powershell.exe -file .\PATH_HERE\autobuild.ps1
+
+License: See [LICENSE.md](LICENSE.md) file
---
+++ autobuild.ps1
@@ -0,0 +1,59 @@
+# Made by Helio <insign@gmail.com> to One Focus
+# Copy to me: .\autobuild.ps1
+# Copy to me (once): Set-ExecutionPolicy RemoteSigned -scope CurrentUser
+
+$git_path = "$env:LOCALAPPDATA\GitHub\PortableGit_ca477551eeb4aea0e4ae9fcd3358bd96720bb5c8\cmd\git.exe"
+
+$project_folder  = "X:\Dropbox\Sites\controle de versao\ponto.me-mobile-app"
+$url_after = "https://build.phonegap.com/?apps/270320/push"
+
+$author = "Helio Oliveira"
+$email = "insign@gmail.com"
+
+$git_branch = "master"
+
+################################################################################
+clear
+
+#Short path to avoid errors
+$git_path = (New-Object -ComObject Scripting.FileSystemObject).GetFile("$git_path").ShortPath
+
+$(Invoke-Expression "$git_path config --global user.email '$email'")
+$(Invoke-Expression "$git_path config --global user.name '$author'")
+
+
+# Ask about the commit message (too slow when use environment variables)
+#[System.Reflection.Assembly]::LoadWithPartialName('Microsoft.VisualBasic') | Out-Null
+#$commit_msg = [Environment]::GetEnvironmentVariable("last_commit_msg","User")
+#$commit_msg = [Microsoft.VisualBasic.Interaction]::InputBox("What's the news?", "Commit message", $commit_msg)
+#[Environment]::SetEnvironmentVariable("last_commit_msg", $commit_msg, "User")
+
+$commit_msg = 'update';
+
+# Open the folder
+cd $project_folder
+
+# Select the branch
+$(Invoke-Expression "$git_path checkout $git_branch")
+
+# Add all new files and remove inexistents
+$(Invoke-Expression "$git_path add -A -v")
+
+# Make the commit
+$(Invoke-Expression "$git_path commit -a -v -m '$commit_msg'")
+
+# Sync commits
+$(Invoke-Expression "$git_path push -v")
+
+# Open URL after all and play sound
+Invoke-Expression "cmd.exe /C start $url_after"
+[System.Media.SystemSounds]::Asterisk.Play()
+
+#clear
+Write-Host "  .______       _______     ___       _______  ____    ____  __   "
+Write-Host "  |   _  \     |   ____|   /   \     |       \ \   \  /   / |  |  "
+Write-Host "  |  |_)  |    |  |__     /  ^  \    |  .--.  | \   \/   /  |  |  "
+Write-Host "  |      /     |   __|   /  /_\  \   |  |  |  |  \_    _/   |  |  "
+Write-Host "  |  |\  \----.|  |____ /  _____  \  |  '--'  |    |  |     |__|  "
+Write-Host "  | _| `._____| |_______/__/     \__\ |_______/     |__|     (__)  "
+Write-Host "  "
